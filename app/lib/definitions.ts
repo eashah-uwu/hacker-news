ac5cd093ee031557b9d982834dcf3bd43eed65f3
Eashah <ebajwa.bscs22seecs@seecs.edu.pk>
2024-09-30 22:55:19 +0500

    first commit

---
+++ app/lib/definitions.ts
@@ -0,0 +1,15 @@
+export type Links = {
+  name: string;
+  path: string;
+};
+
+export type Story = {
+  by: string;
+  descendants: number;
+  id: number;
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+};
---
+++ app/lib/stories.ts
@@ -0,0 +1,54 @@
+export type Story = {
+  by: string;
+  descendants: number;
+  id: number;
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+};
+
+export type Stories = Story[];
+
+export const getStories = async (
+  data: number[],
+  page_number: number
+): Promise<Stories> => {
+  const page_size = 30;
+  const start = page_number * page_size;
+  const end = start + page_size;
+
+  const fetches = await Promise.all(
+    data
+      .slice(start, end)
+      .map(async (id: number) =>
+        fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/item/${id}.json?print=pretty`
+        )
+      )
+  );
+
+  const stories = await Promise.all(fetches.map((f) => f.json()));
+
+  return stories;
+};
+
+export const getAllStories = async (data: number[]): Promise<Stories> => {
+  const fetches = await Promise.all(
+    data.map(async (id: number) =>
+      fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/item/${id}.json?print=pretty`)
+    )
+  );
+
+  const stories = await Promise.all(fetches.map((f) => f.json()));
+
+  return stories;
+};
+
+export const getIDs = async (): Promise<number[]> => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
+  const data = await response.json();
+
+  return data;
+};
